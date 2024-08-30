58cc9dfebf697b54c0b185055c6d9378e4bdb24a
natalie <nataliey2027@student.cis.edu.hk>
2024-08-30 13:03:41 +0800

    fun lab

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,28 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Natalie
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favoriteFood;
 
+        System.out.println("What is your age:\t");
+        age = scanner.nextInt();
 
+        System.out.println("What is your first name:\t");
+        firstName = scanner.next();
 
+        System.out.println("What is your favorite food:\t");
+        favoriteFood = scanner.next();
 
+        System.out.printf(" First name:%s\n Age:%d \n Favorite Food:%s",firstName,age,favoriteFood);
     }
 
 }
